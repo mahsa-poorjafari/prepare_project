982ad8bf01d6a79a467aa4b8b67f031d00394a93
mahsa <m_mw7462@yahoo.com>
2014-04-21 13:25:36 +0430

    delete scaffold for fix error

--- app/views/categories/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@categories) do |category|
-  json.extract! category, :id, :title, :description
-  json.url category_url(category, format: :json)
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 PrepareProject::Application.routes.draw do
 
-  resources :categories
-
   root :to => 'static#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
