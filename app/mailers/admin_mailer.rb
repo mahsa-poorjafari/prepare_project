190033d3706536be75e25b143c9fb1121bb091e0
mohamamd <mb.sepanta@gmail.com>
2014-04-22 18:17:48 +0430

    fix propblem in sending mails

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,9 +1,11 @@
 class AdminMailer < ActionMailer::Base
   
   def send_user_mail
-    p 'ssssssssssssssssssssssssss start sending'
-    mail(to: 'mahsa.poorjafari@gmail.com', subject: 'prepared')
-    p 'fffffffffffffffffffffff finish '
+    @message = Message.last
+    mail(:to => "mb.sepanta@gmail.com", :subject => "Registered", :from => "mb.sepanta@gmail.com")
+  end
+  def send_hi
+    mail(:to => "mb.sepanta@gmail.com", :subject => "Registered", :from => "mb.sepanta@gmail.com")
   end
   
 end
