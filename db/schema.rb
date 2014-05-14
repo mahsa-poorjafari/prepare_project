e8b458a3dea7c258ff45cbb2867edae595c80b9a
mahsa <m_mw7462@yahoo.com>
2014-05-14 15:35:50 +0430

    add del option for category and add coming soon option for model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507052232) do
+ActiveRecord::Schema.define(version: 20140514095704) do
 
   create_table "categories", force: true do |t|
     t.string   "title"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20140507052232) do
     t.datetime "updated_at"
     t.string   "title_fa"
     t.text     "description_fa"
+    t.boolean  "coming_soon"
   end
 
   create_table "resellers", force: true do |t|
