cbd4132132313fc655d5a47d2516a8889d348748
mahsa <m_mw7462@yahoo.com>
2014-06-12 13:14:35 +0430

    set show pictures of som product order as ASC

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.order( "title_en DESC")
     @category = Category.all
   end
 
