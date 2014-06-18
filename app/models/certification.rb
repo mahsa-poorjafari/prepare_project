85114831be70d5910e4231fc27d9d330ef77c4d8
mahsa <m_mw7462@yahoo.com>
2014-06-18 11:20:54 +0430

    remove arrow for uo and down and set scroll

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 class Certification < ActiveRecord::Base  
-  validates :title_fa, :title_en, :presence => {:message => 'عنوان گواهینامه را وارد کنید.'}
+  
   belongs_to :category
   has_attached_file :image, :styles => { :large => "1000x1000>" , :medium => "300x300>", :small => "200x200>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png" , "image/gif"],
