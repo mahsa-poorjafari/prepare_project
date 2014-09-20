ed67225771fad8d5f615f2fd209b720e85e5d705
mahsa <m_mw7462@yahoo.com>
2014-09-20 15:34:42 +0430

    remove friendly id from pictures/controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -64,7 +64,7 @@ class PicturesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_picture
-      @picture = Picture.friendly.find(params[:id])
+      @picture = Picture.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
