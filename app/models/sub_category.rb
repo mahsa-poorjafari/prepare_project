72a21e5b79de0f4ec944d125bf4d3d26f7e1be56
mahsa <m_mw7462@yahoo.com>
2014-09-20 10:54:25 +0430

    change original size to large

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -4,7 +4,7 @@ class SubCategory < ActiveRecord::Base
   friendly_id :title_fa
   belongs_to :category
   has_many :products
-  has_attached_file :image, :styles => { :original => "500x500>" , :medium => "300x300>", :small => "200x200#" }
+  has_attached_file :image, :styles => { :large => "500x500>" , :medium => "300x300>", :small => "200x200#" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"],
                                             :message =>  "فرمت عکس صحیح نیست"
   
